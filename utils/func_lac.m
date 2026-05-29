7dfdccf1cb20e63c4cce1ab0ddc3166705ea4302
Junaid <mhmmd.junaid.ali@gmail.com>
2026-05-30 01:23:22 +0500

    Refactor LAC calculation for improved clarity

--- utils/func_lac.m
+++ utils/func_lac.m
@@ -1,10 +1,6 @@
 function [LAC,LAC2DMAT,OverallLAC] = func_lac(Y_1,Y_2)
 
-numerator = 2*(abs((conj(Y_1).*Y_2)));
-denumerator = (Y_1 .* conj(Y_1)) + (Y_2 .* conj(Y_2));
-LAC = numerator ./ denumerator;
+LAC = 2*(abs((conj(Y_1).*Y_2))) ./ ((Y_1 .* conj(Y_1)) + (Y_2 .* conj(Y_2)));
 
 OverallLAC=mean(LAC,'all');
 LAC2DMAT=mean(LAC,3);
-
-
